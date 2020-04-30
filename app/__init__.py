e7b4216959d6c66680979ce4d391c37a719cbad5
Cronos99 <fchristopher454@gmail.com>
2020-04-30 16:04:47 +0100

    No

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+maps = ['Border', 'Chalet']
+
+
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,38 @@
-from app import app
+from app import app, maps
 from flask import render_template
+import os
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', username=['Chris', 'Jake'])
+    return render_template('index.html', maps=maps)
+
+@app.route('/<mapname>')
+def user(mapname):
+    
+    path = './app/'
+    sec_path = '../../app/'
+    sites = []
+    site = 0
+    completeSites = []
+    imageUrls = {}
+
+    if mapname != 'favicon.ico':
+        for dirname in os.listdir(path + mapname):
+            complete = os.path.join(path, mapname, dirname)
+            if os.path.isdir(complete):
+                sites.append(dirname)
+                completeSites.append(complete)
+
+                for filename in os.listdir(complete):
+                    if site in imageUrls:
+                        imageUrls[site].append(filename)
+                    else:
+                        imageUrls.setdefault(site, [filename])
+            
+                site += 1
+
+
+        
+
+    return render_template('default_map.html', map=mapname, sites=sites, imageUrls = imageUrls)
